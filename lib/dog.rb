c78a13ab59a2844cec684cc20a2e37abe39b5d12
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-08-18 08:14:11 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
-class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
-  end
+require 'pry'
 
-  def name
-    this_dogs_name
-  end
+class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    
+    def name
+        @this_dogs_name
+    end
 end
+
+
+
+lassie = Dog.new
+
+lassie.name 
+
